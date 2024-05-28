ed1832a6dc5cb9d5121b9d8f5d1e4b49270eeeef
LeeEndl <127639272+LeeEndl@users.noreply.github.com>
2024-05-27 22:48:15 -0500

    implied logging on

--- main.cpp
+++ main.cpp
@@ -19,12 +19,9 @@
 
 #include <enet/compress.c> // TODO
 
-using pr = std::vector<int>;
-
 template<typename T>
-void shift_pos(std::vector<std::byte>& buffer, short& pos, T& value) {
-    if (pos >= 0x7fff) return;
-    std::copy(buffer.begin() + pos, buffer.begin() + pos + sizeof(T), reinterpret_cast<std::byte*>(&value));
+void shift_pos(std::vector<std::byte>& data, short& pos, T& value) {
+    std::copy(data.begin() + pos, data.begin() + pos + sizeof(T), reinterpret_cast<std::byte*>(&value));
     pos += sizeof(T);
 }
 
@@ -41,21 +38,21 @@ int main() {
 
     std::ifstream file("items.dat", std::ios::binary | std::ios::ate);
         std::streamsize size = file.tellg();
-        std::vector<std::byte> buffer(size + 60);
-            std::fill(buffer.begin(), buffer.begin() + 60, std::byte{0});
-            pr info{0x4, 0x10, -1, 0x8};
-                for (char i = 0; i < info.size(); ++i) 
-                    std::copy(reinterpret_cast<std::byte*>(&info[i]), reinterpret_cast<std::byte*>(&info[i]) + sizeof(info[i]), buffer.begin() + i * 4);
-            std::copy(reinterpret_cast<std::byte*>(&size), reinterpret_cast<std::byte*>(&size) + sizeof(size), buffer.begin() + 56);
+        std::vector<std::byte> data(size + 60);
+            std::fill(data.begin(), data.begin() + 60, std::byte{0});
+            std::array<int, 4> buffer{0x4, 0x10, -1, 0x8};
+                for (char i = 0; i < buffer.size(); ++i) 
+                    std::copy(reinterpret_cast<std::byte*>(&buffer[i]), reinterpret_cast<std::byte*>(&buffer[i]) + sizeof(buffer[i]), data.begin() + i * 4);
+            std::copy(reinterpret_cast<std::byte*>(&size), reinterpret_cast<std::byte*>(&size) + sizeof(size), data.begin() + 56);
             file.seekg(0, std::ios::beg);
-            file.read(reinterpret_cast<char*>(buffer.data() + 60), size);
-            auto span = std::span<const unsigned char>(reinterpret_cast<const unsigned char*>(buffer.data()), buffer.size());
+            file.read(reinterpret_cast<char*>(data.data() + 60), size);
+            auto span = std::span<const unsigned char>(reinterpret_cast<const unsigned char*>(data.data()), data.size());
                 int hash = std::accumulate(span.begin(), span.end(), 0x55555555u, 
                     [](auto start, auto end) { return (start >> 27) + (start << 5) + end; });
 
         short pos{60}, count{0}, version{0};
-        shift_pos(buffer, pos, version);
-        shift_pos(buffer, pos, count);
+        shift_pos(data, pos, version);
+        shift_pos(data, pos, count);
         for (short i = 0; i < count; i++) {
 
         }
@@ -64,7 +61,7 @@ int main() {
 
     ENetEvent event{};
     while (true) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
         while (enet_host_service(server, &event, 1000) > 0)
             std::jthread([&](std::stop_token handle)
 	        {
@@ -73,11 +70,10 @@ int main() {
                 {
 				    case ENET_EVENT_TYPE_CONNECT: 
                     {
-                        std::vector<int> buffer{1, 0, 0};
-                        const auto packet = enet_packet_create(nullptr, buffer[1] + 5, ENET_PACKET_FLAG_RELIABLE);
-                        std::copy(reinterpret_cast<std::byte*>(&buffer[0]), reinterpret_cast<std::byte*>(&buffer[0]) + 4, packet->data);
-                        std::copy(reinterpret_cast<std::byte*>(&buffer[2]), reinterpret_cast<std::byte*>(&buffer[2]) + 1, packet->data + 2 + buffer[1]);
-                        enet_peer_send(event.peer, 0, packet);
+                        const auto packet = enet_packet_create(nullptr, 5, ENET_PACKET_FLAG_RELIABLE);
+                            std::array<int, 1> buffer{1};
+                            std::copy(reinterpret_cast<enet_uint8*>(&buffer[0]), reinterpret_cast<enet_uint8*>(&buffer[0]) + 4, packet->data);
+                            enet_peer_send(event.peer, 0, packet);
                         break;
                     }
                     case ENET_EVENT_TYPE_DISCONNECT: 

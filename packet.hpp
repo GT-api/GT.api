08ae4bb430b58117ab3961de8ccf447f09e93070
LeeEndl <gavinolson71@gmail.com>
2024-06-02 20:08:36 -0500

    major changes in `gt_packet()`

--- packet.hpp
+++ packet.hpp
@@ -1,12 +1,7 @@
+/* parent: peer.hpp */
+#include <cstring> /* std::strlen() */
 
-#include <enet/packet.c>
-#include <array>
-#include <numeric>
-#include <memory>
-#include <vector> 
-#include <cstring>
-#include <algorithm>
-#include <type_traits>
+#include "include/enet.h"
 
 /*
 @param peer to whom this packet is sent to.
@@ -24,46 +19,49 @@ void gt_packet(ENetEvent event, signed/*unsigned...*/ wait_for, T... params) {
             for (short ii = 0; ii < sizeof(int); ++ii)
                 (data.get() + offsets[i])[ii] = reinterpret_cast<const std::byte*>(&buffer[i])[ii];
         }
-    int index = 0, len = 61;
+    size_t size = 61;
+    std::byte index{0x0};
     std::apply([&](auto const&... param) {
         (..., (void)([&]()
         {
-            LOG(std::format("Index: {0}", index));
+            LOG(std::format("Index: {0}", std::to_integer<int>(index)));
             if constexpr (std::is_same_v<std::decay_t<decltype(param)>, const char*>) {
                 LOG(std::format("String: {0}; Size: {1}", param, std::strlen(param)));
-                size_t new_size = len + 2 + std::strlen(param) + sizeof(int);
-                auto this_data = std::make_unique_for_overwrite<std::byte[]>(new_size);
-                std::ranges::copy(data.get(), data.get() + len, this_data.get());
-                this_data[len] = static_cast<std::byte>(index);
-                this_data[len + 1] = std::byte{0x2};
-                std::ranges::copy(std::bit_cast<std::array<std::byte, sizeof(int)>>(static_cast<int>(std::strlen(param))), this_data.get() + len + 2);
-                std::ranges::copy(std::as_bytes(std::span(param, static_cast<int>(std::strlen(param)))), this_data.get() + len + 6);
-                len = new_size;
+                auto this_data = std::make_unique_for_overwrite<std::byte[]>(size + 2 + std::strlen(param) + sizeof(int));
+                for (size_t i = 0; i < size; ++i)
+                    this_data[i] = data[i];
+                this_data[size] = index; /* element counter. e.g. "OnConsoleMessage" -> 0x0, "the console message" -> 0x1 */
+                this_data[size + 1] = std::byte{0x2};
+                this_data[size + 2] = static_cast<std::byte>(std::strlen(param)); /* e.g. "OnConsoleMessage" -> 0x16 */
+                for (int i = 0; i < 3; ++i) 
+                    this_data[size + 3 + i] = std::byte{0x0}; /* 0x0 0x0 0x0 */
+                for (size_t i = 0; i < std::strlen(param); ++i) 
+                    this_data[size + 6 + i] = static_cast<std::byte>(param[i]); /* e.g. set byte to each char. e.g. 'a' -> 0x97. 'z' = 0x122 */
+                size = size + 2 + std::strlen(param) + sizeof(int);
                 data = std::move(this_data);
             }
             else if constexpr (std::is_signed_v<std::decay_t<decltype(param)>> or std::is_unsigned_v<std::decay_t<decltype(param)>>) {
                 LOG(std::format("Integer: {0}; Type: {1}", param, (std::is_signed_v<std::decay_t<decltype(param)>>) ? "signed" : "unsigned"));
-                size_t new_size = len + 2 + sizeof(int);
-                auto this_data = std::make_unique_for_overwrite<std::byte[]>(new_size);
-                std::ranges::copy(data.get(), data.get() + len, this_data.get());
-                this_data[len] = static_cast<std::byte>(index);
-                this_data[len + 1] = std::byte{(std::is_signed_v<std::decay_t<decltype(param)>>) ? 0x9 : 0x5};
-                auto i_data = std::as_bytes(std::span(&param, 1));
-                std::ranges::copy(const_cast<std::byte*>(i_data.data()), const_cast<std::byte*>(i_data.data()) + i_data.size(), this_data.get() + len + 2);
-
-                len = new_size;
+                auto this_data = std::make_unique_for_overwrite<std::byte[]>(size + 2 + sizeof(int));
+                for (size_t i = 0; i < size; ++i)
+                    this_data[i] = data[i];
+                this_data[size] = index; /* element counter. e.g. "OnConsoleMessage" -> 0x0, 43562/-43562 -> 0x1 */
+                this_data[size + 1] = std::byte{(std::is_signed_v<std::decay_t<decltype(param)>>) ? 0x9 : 0x5}; 
+                for (int i = 0; i < sizeof(param); ++i)
+                    this_data[size + 2 + i] = reinterpret_cast<std::byte const*>(&param)[i];
+                size = size + 2 + sizeof(int);
                 data = std::move(this_data);
             }
-            ++index;
-		    if (len >= 61) 
+            index = static_cast<std::byte>(std::to_integer<int>(index) + 1);
+		    if (size >= 61) 
                 data[60] = static_cast<std::byte>(index);
         }()
     ));
     }, std::make_tuple(params...));
-        ENetPacket* packet = enet_packet_create(data.get(), len, ENET_PACKET_FLAG_RELIABLE);
+        ENetPacket* packet = enet_packet_create(data.get(), size, ENET_PACKET_FLAG_RELIABLE);
             if (packet not_eq NULL)
 	            if (enet_peer_send(event.peer, 0, packet) == 0) 
-                     LOG(std::format("Released Packet; Name: {0}; Size: {1} bytes", std::get<0>(std::make_tuple(params...)), len));
-                else LOG(std::format("Invalid Packet; Name: {0}; Size: {1} bytes", std::get<0>(std::make_tuple(params...)), len) );
+                     LOG(std::format("Released Packet; Name: {0}; Size: {1} bytes", std::get<0>(std::make_tuple(params...)), size));
+                else LOG(std::format("Invalid Packet; Name: {0}; Size: {1} bytes", std::get<0>(std::make_tuple(params...)), size) );
     
 };

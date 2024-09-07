30a1528599d4561c3019911ea9f35bf3f4471d06
LeeEndl <gavinolson71@gmail.com>
2024-09-07 18:03:41 -0500

    input extra space filtering

--- include/action/input.hpp
+++ include/action/input.hpp
@@ -3,6 +3,13 @@ void input(ENetEvent event, const std::string& header)
 {
     if (not create_rt(event, 1, 400ms)) return;
     std::string text{readch(std::string{header}, '|')[4]};
+    {
+        auto not_isspace = [](char c){ return not std::isspace(static_cast<unsigned char>(c)); };
+        std::string::iterator start = std::ranges::find_if(text, not_isspace);
+        std::string::iterator end = std::ranges::find_if(text.rbegin(), text.rend(), not_isspace).base();
+        if (text.empty() or start >= end) return;
+        text = std::string{start, end};
+    }
     _peer[event.peer]->messages.push_back(steady_clock::now());
     if (_peer[event.peer]->messages.size() > 5) _peer[event.peer]->messages.pop_front();
     if (_peer[event.peer]->messages.size() == 5 and std::chrono::duration_cast<seconds>(steady_clock::now() - _peer[event.peer]->messages.front()).count() < 6)
@@ -11,10 +18,13 @@ void input(ENetEvent event, const std::string& header)
     else if (text.starts_with('/')) 
     {
         packet(*event.peer, std::format("action|log\nmsg| `6{}``", text).c_str());
-        if (cmd_pool.contains(text.substr(1, text.size())))
-            (static_cast<void>(std::async(std::launch::async, cmd_pool[text.substr(1, text.size())], std::ref(event), std::move(text.substr(1, text.size())))));
+        if (cmd_pool.contains(text.substr(1, text.length())))
+            (static_cast<void>(std::async(std::launch::async, cmd_pool[text.substr(1, text.length())], std::ref(event), std::move(text.substr(1, text.length())))));
         else packet(*event.peer, "action|log\nmsg|`4Unknown command.``  Enter `$/?`` for a list of valid commands.");
     }
+    else if (text.back() == ' ' and text.length() > 1) text.pop_back(); // @note trim back spaces. "test "
+    else if (text.front() == ' ' and text.length() > 1) text = text.substr(1, text.length() - 1); // @note trim front spacing. " test"
+    else if (text.empty()) return; // @note ignore empty messages.
     else peers(ENET_PEER_STATE_CONNECTED, [&](ENetPeer& p) 
     {
         if (not _peer[&p]->recent_worlds.empty() and not _peer[event.peer]->recent_worlds.empty() and _peer[&p]->recent_worlds.back() == _peer[event.peer]->recent_worlds.back())

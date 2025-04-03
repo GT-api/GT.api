0cba25b73274e69f52fbaa6fb980c8e43e17757c
LeeEndl <gavinolson71@gmail.com>
2025-04-03 18:51:29 -0500

    support older items.dat

--- include/database/items.hpp
+++ include/database/items.hpp
@@ -39,75 +39,122 @@ using namespace std::chrono;
 void cache_items() 
 {
     time_start
-    int pos{60}, count{0};
-    pos += sizeof(short); // @note items.dat version.
-    shift_pos(im_data, pos, count);
-    for (int i = 0; i < count; ++i) 
-    {
-        item im{};
-        shift_pos(im_data, pos, im.id); pos += 2; // @note downsize im.id to 2 bit rather then a 4 bit
-        pos += 2;
-        shift_pos(im_data, pos, im.type); pos -= 1; // @note upsize im.type to 2 bit rather then a 1 bit
-        pos += 1;
-        short len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short);
-        im.raw_name.resize(len);
-        for (short i = 0; i < len; ++i) 
-            im.raw_name[i] = static_cast<char>(im_data[pos] ^ std::byte(std::string_view{"PBG892FXX982ABC*"}[(i + im.id) % std::string_view{"PBG892FXX982ABC*"}.length()])), 
-            ++pos;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        pos += 14;
-        unsigned char raw_hits{};
-        shift_pos(im_data, pos, raw_hits);
-        im.hits = static_cast<short>(raw_hits);
-        if (im.hits not_eq 0) im.hits /= 6; // @note unknown reason behind why break hit is muliplied by 6 then having to divide by 6
-        pos += sizeof(int);
-        unsigned char cloth_type{};
-        shift_pos(im_data, pos, cloth_type);
-        im.cloth_type = static_cast<unsigned short>(cloth_type);
-        pos += 3;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        pos += 8;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        pos += 24;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        pos += 80;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len; // version 11
-        pos += 13; // version 12
-        pos += 8; // version 13, 14
-
-        /* version 15 */
-        pos += 25;
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len;
-        
-        len = *(reinterpret_cast<short*>(&im_data[pos]));
-        pos += sizeof(short) + len; // version 16
-        pos += 8; // version 17, 18
-        pos += 9; // version 19
-        pos += 2; // version 21
-        if (im.id % 2 == 0) 
+    try {
+        int pos{60}, count{};
+        short version{};
+        shift_pos(im_data, pos, version);
+        printf("items.dat version: %d\n", version);
+        shift_pos(im_data, pos, count);
+        for (int i = 0; i < count; ++i) 
         {
-            if (std::string lower = im.raw_name | std::ranges::views::transform(
-                [&](char c) { return std::tolower(c); }) bitor std::ranges::to<std::string>(); lower.contains("ancestral"))
-                    im.cloth_type = clothing::ances;
+            item im{};
+            
+            shift_pos(im_data, pos, im.id); pos += 2; // @note downsize im.id to 2 bit rather then a 4 bit
+            pos += 2;
+
+            shift_pos(im_data, pos, im.type); pos -= 1; // @note upsize im.type to 2 bit rather then a 1 bit
+            pos += 1;
+
+            short len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short);
+            im.raw_name.resize(len);
+            for (short i = 0; i < len; ++i) 
+                im.raw_name[i] = static_cast<char>(im_data[pos] ^ std::byte(std::string_view{"PBG892FXX982ABC*"}[(i + im.id) % std::string_view{"PBG892FXX982ABC*"}.length()])), 
+                ++pos;
+
+            len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short) + len;
+
+            pos += 14;
+
+            unsigned char raw_hits{};
+            shift_pos(im_data, pos, raw_hits);
+            im.hits = static_cast<short>(raw_hits);
+            if (im.hits not_eq 0) im.hits /= 6; // @note unknown reason behind why break hit is muliplied by 6 then having to divide by 6
+            
+            pos += sizeof(int);
+
+            unsigned char cloth_type{};
+            shift_pos(im_data, pos, cloth_type);
+            im.cloth_type = static_cast<unsigned short>(cloth_type);
+
+            pos += 3;
+
+            len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short) + len;
+
+            pos += 8;
+
+            len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short) + len;
+
+            len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short) + len;
+
+            len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short) + len;
+
+            len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short) + len;
+
+            pos += 24;
+
+            len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short) + len;
+
+            len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short) + len;
+
+            len = *(reinterpret_cast<short*>(&im_data[pos]));
+            pos += sizeof(short) + len;
+
+            pos += sizeof(std::array<std::byte, 80>);
+            if (version < 11) throw std::runtime_error("GT.api does not support items.dat less than version 11.");
+            if (version >= 11)
+            {
+                pos += *(reinterpret_cast<short*>(&im_data[pos]));
+                pos += sizeof(short);
+            }
+            if (version >= 12)
+            {
+                pos += sizeof(int);
+                pos += sizeof(std::array<std::byte, 9>);
+            }
+            if (version >= 13)
+                pos += sizeof(int);
+            if (version >= 14)
+                pos += sizeof(int);
+            if (version >= 15)
+            {
+                pos += sizeof(std::array<std::byte, 25>);
+                pos += *(reinterpret_cast<short*>(&im_data[pos]));
+                pos += sizeof(short);
+            }
+            if (version >= 16)
+            {
+                pos += *(reinterpret_cast<short*>(&im_data[pos]));
+                pos += sizeof(short);
+            }
+            if (version >= 17)
+                pos += sizeof(int);
+            if (version >= 18)
+                pos += sizeof(int);
+            if (version >= 19)
+                pos += sizeof(std::array<std::byte, 9>);
+            if (version == 21)
+                pos += sizeof(short);
+            if (version > 21) throw std::runtime_error("GT.api does not support items.dat greater than version 21.");
+            if (im.id % 2 == 0) 
+            {
+                if (std::string lower = im.raw_name | std::ranges::views::transform(
+                    [&](char c) { return std::tolower(c); }) bitor std::ranges::to<std::string>(); lower.contains("ancestral"))
+                        im.cloth_type = clothing::ances;
+            }
+            items.emplace(i, im);
         }
-        items.emplace(i, im);
+    } catch (const std::exception& exc) {
+        time_end(std::format("catch error in cache_items() [{}]", exc.what()).c_str())
+        return;
     }
     time_end("void cache_items()")
 }

88c4639919b4081ee042307a5f6ba3c5357b2c52
LeeEndl <gavinolson71@gmail.com>
2025-04-08 15:35:27 -0500

    organized main.cpp
    also added error handling for opening items.dat due to many people asking for help on this specfic error.

--- main.cpp
+++ main.cpp
@@ -22,7 +22,11 @@ int enet_host_compress_with_range_coder(ENetHost* host); // -> import compress.o
 int main() 
 {
     {
-        ENetCallbacks callbacks{.malloc = &malloc, .free = &free, .no_memory = []() {printf("enet memory overflow");}}; // @note capture message on no_memory
+        ENetCallbacks callbacks{
+            .malloc = &malloc, 
+            .free = &free, 
+            .no_memory = []() { printf("enet memory overflow"); }
+        };
         enet_initialize_with_callbacks(ENET_VERSION, &callbacks);
     }
     server = enet_host_create({.host = in6addr_any, .port = 17091}, ENET_PROTOCOL_MAXIMUM_PEER_ID, ENET_PROTOCOL_MINIMUM_CHANNEL_COUNT);
@@ -30,11 +34,23 @@ int main()
         enet_host_compress_with_range_coder(server);
     {
         std::ifstream file("items.dat", std::ios::binary bitor std::ios::ate);
-        std::streamsize size = file.tellg(); // @note size of ios::ate (end of file). this is called before seekg (ios::beg (beginning of file)).
+        if (not file.is_open()) {
+            printf("failed to open items.dat");
+            getchar();
+        }
+        std::streampos size = file.tellg(); // @note size of ios::ate (end of file). this is called before seekg (ios::beg (beginning of file)).
         im_data.resize(im_data.size() + size);
-        *reinterpret_cast<std::array<unsigned char, 56>*>(&im_data[0]) = {0x4, 0x0, 0x0, 0x0, 0x10, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x8};
-        *reinterpret_cast<std::streamsize*>(&im_data[56]) = size;
-        file.seekg(0, std::ios::beg).read(reinterpret_cast<char*>(&im_data[60]), size);
+        *reinterpret_cast<std::array<unsigned char, 20>*>(&im_data[0]) = {
+            0x4, 0x0, 0x0, 0x0, 
+            0x10, 0x0, 0x0, 0x0, 
+            0xFF, 0xFF, 0xFF, 0xFF, 
+            0x0, 0x0, 0x0, 0x0, 
+            0x8, 0x0, 0x0, 0x0
+        };
+        *reinterpret_cast<std::streampos*>(&im_data[20 + 36]) = size;
+        file
+            .seekg(0, std::ios::beg) // @note  start from beginning of items.dat
+            .read(reinterpret_cast<char*>(&im_data[60]), size);
     } // @note delete & close file
     cache_items();
 

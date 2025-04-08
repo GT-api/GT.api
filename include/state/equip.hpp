f6ea436ae2c9e6eb56d8554ce569e9890bcd42de
LeeEndl <gavinolson71@gmail.com>
2025-04-07 20:08:09 -0500

    add unequip feature
    added some comments for people who wanna learn a little.

--- include/state/equip.hpp
+++ include/state/equip.hpp
@@ -1,9 +1,15 @@
 
 void equip(ENetEvent event, state state)
 {
+    /* checks if item is clothing. */
     if (items[state.id].cloth_type == clothing::none) return;
 
-    _peer[event.peer]->clothing[items[state.id].cloth_type] = state.id;
+    /* checks if clothing is already equipped. if so unequip. else equip. */
+    if (_peer[event.peer]->clothing[items[state.id].cloth_type] == state.id)
+        _peer[event.peer]->clothing[items[state.id].cloth_type] = 0;
+    else
+        _peer[event.peer]->clothing[items[state.id].cloth_type] = state.id;
 
+    /* update visuals. */
     clothing_visuals(event);
 }

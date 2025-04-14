24d5dedc75582149c3fc4edbbdde2b351bf0bc19
LeeEndl <gavinolson71@gmail.com>
2025-04-13 20:31:43 -0500

    refresh_item_data packet is sent reliable.

--- include/action/refresh_item_data.hpp
+++ include/action/refresh_item_data.hpp
@@ -2,5 +2,5 @@
 void refresh_item_data(ENetEvent event, const std::string& header) 
 {
     if (_peer[event.peer]->recent_worlds.back().empty())
-        enet_peer_send(event.peer, 0, enet_packet_create(im_data.data(), im_data.size(), ENET_PACKET_FLAG_NO_ALLOCATE));
+        enet_peer_send(event.peer, 0, enet_packet_create(im_data.data(), im_data.size(), ENET_PACKET_FLAG_RELIABLE));
 }
